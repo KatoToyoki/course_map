58ebded182020c12fddc44067d196e35d4c6098c
tyk <110590015@ntut.org.tw>
2023-01-17 15:01:59 +0800

    tidy up

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Col, Row } from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                test
+            </h1>
+        </div>
+    )
+}
