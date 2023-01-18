cb7dbafbba2a5d97b062bf7f8cec76721858351a
tyk <110590015@ntut.org.tw>
2023-01-18 18:03:24 +0800

    map and course okay

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,12 +1,10 @@
 import { Col, Row } from "react-bootstrap";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 import '../css/style.css'
-
+import '../css/map.css'
 import { category } from '../data/map_data'
-
 
 export default function Course() {
     const id = (window.location.href).slice(-2),
@@ -25,90 +23,98 @@ export default function Course() {
     }
 
     return (
-        <div className="inner_container">
-            <Row>
-                <Col sm={3} className='center'>
-                    <h5>
-                        【{difficulty}課程】
-                    </h5>
-                </Col>
-                <Col sm={9}>
-                    <h2>
-                        {data.course_name}
-                    </h2>
-                </Col>
-                <hr></hr>
-                <Col sm={12} className='yellow_word'>
-                    <p><b>{data.course_depiction}</b></p>
-                </Col>
-                <Col sm={4} className='green inside'>
-                    核心能力
-                </Col>
-                <Col sm={8} className='light_green inside'>
-                    <Row>
-                        <Col sm={3} className='center'>
-                            {data.cognition}
-                        </Col>
-                        <Col sm={3} className='center'>
-                            {data.knowledge}
-                        </Col>
-                        <Col sm={3} className='center'>
-                            {data.technology}
-                        </Col>
-                        <Col sm={3} className='center'>
-                            {data.value}
-                        </Col>
-                    </Row>
-                </Col>
+        < div className="course_container" >
+            <div className="course_bg">
+                <Row>
+                    <Col sm={3} className='center'>
+                        <h5>
+                            【{difficulty}課程】
+                        </h5>
+                    </Col>
+                    <Col sm={9}>
+                        <h2>
+                            {data.course_name}
+                        </h2>
+                    </Col>
+                    <hr></hr>
+                    <Col sm={12} className='yellow_word'>
+                        <p><b>{data.course_depiction}</b></p>
+                    </Col>
+                    <Col sm={4} className='light_green inside'>
+                        核心能力
+                    </Col>
+                    <Col sm={8} className='green inside'>
+                        <Row>
+                            <Col sm={3} className='center'>
+                                {data.cognition}
+                            </Col>
+                            <Col sm={3} className='center'>
+                                {data.knowledge}
+                            </Col>
+                            <Col sm={3} className='center'>
+                                {data.technology}
+                            </Col>
+                            <Col sm={3} className='center'>
+                                {data.value}
+                            </Col>
+                        </Row>
+                    </Col>
 
-                <Col sm={4} className='inside'>
-                    開課時間
-                </Col>
-                <Col sm={8} className='inside'>
-                    {data.course_time.map((time) => {
-                        return (
-                            <Row>
-                                <Col sm={6}>
-                                    課號：{time.id}
-                                </Col>
-                                <Col sm={6}>
-                                    {time.day} &nbsp; {time.time}
-                                </Col>
-                                <Col sm={12}>
-                                    <br />
-                                </Col>
-                            </Row>
-                        );
-                    })}
-                </Col>
+                    <Col sm={4} className='inside'>
+                        開課時間
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.course_time.map((time) => {
+                            return (
+                                <Row>
+                                    <Col sm={6}>
+                                        課號：{time.id}
+                                    </Col>
+                                    <Col sm={6}>
+                                        {time.day} &nbsp; {time.time}
+                                    </Col>
+                                    <Col sm={12}>
+                                        <br />
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
 
-                <Col sm={4} className='inside'>
-                    學分數
-                </Col>
-                <Col sm={8} className='inside'>
-                    {data.credit}學分，每週{data.credit}小時
-                </Col>
-                <Col sm={12}>
-                    <br />
-                </Col>
+                    <Col sm={4} className='inside'>
+                        學分數
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.credit}學分，每週{data.credit}小時
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
 
-                <Col sm={4} className='inside'>
-                    開課系級
-                </Col>
-                <Col sm={8} className='inside'>
-                    {data.department}
-                </Col>
-                <Col sm={12}>
-                    <br />
-                </Col>
+                    <Col sm={4} className='inside'>
+                        開課系級
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.department}
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
 
-                <Col sm={4} className='inside'>
-                    授課教師
-                </Col>
-                <Col sm={8} className='inside'>
-                    {data.lecturer}
-                </Col>
-            </Row>
+                    <Col sm={4} className='inside'>
+                        授課教師
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.lecturer}
+                    </Col>
+                </Row>
+            </div>
         </div >
     )
 }
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,80 +1,81 @@
-import { Col, Row, Button, Nav } from "react-bootstrap";
-import { Outlet, Link } from 'react-router-dom';
-
+import { Col, Row, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 import '../css/style.css'
 import '../css/map.css'
 import { category } from '../data/map_data'
-// import { courses } from '../data/course_data'
 
 
 export default function Map() {
     return (
-        <div className="inner_container">
-            <h1>
-                課程地圖
-            </h1>
-            <p>通過本微學程，最低應修10學分</p>
-            <br />
-            <div>
-                {category.map((data) => {
-                    return (
-                        <Row>
-                            <Col sm={12} md={2} className='color1'>
-                                <h4>{data.difficulty}</h4>
-                                <p>{data.depiction}</p>
-                            </Col>
-                            <Col sm={12} md={10}>
-                                {data.inner_data.map((datas) => {
-                                    return (
-                                        <Row>
-                                            <Col sm={12} md={6} className='color2'>
+        <div className="map_bg">
+            <div className="map_container">
+                <div className="map_content">
+                    <h1>
+                        課程地圖
+                    </h1>
+                    <p className="yellow_word">通過本微學程，最低應修10學分</p>
+                    <br />
+                    <div>
+                        {category.map((data) => {
+                            return (
+                                <Row>
+                                    <Col sm={12} md={2} className='map_inside'>
+                                        <h4>{data.difficulty}</h4>
+                                        <p>{data.depiction}</p>
+                                    </Col>
+                                    <Col sm={12} md={10}>
+                                        <hr />
+                                        {data.inner_data.map((datas) => {
+                                            return (
                                                 <Row>
-                                                    <Col sm={2} className='center'>
-                                                        {datas.credit}
+                                                    <Col sm={12} md={6} className='map_inside'>
+                                                        <Row>
+                                                            <Col sm={10}>
+                                                                <a href={"/course_map#/course/" + datas["course_id"]} className='map_a'>
+                                                                    <div className="d-grid">
+                                                                        <Button variant="outline-dark" size='lg'>
+                                                                            {datas.course_name}
+                                                                        </Button>
+                                                                    </div>
+                                                                </a>
+                                                            </Col>
+                                                            <Col sm={2} className='center'>
+                                                                <div className="credit blue_green">
+                                                                    {datas.credit}
+                                                                </div>
+                                                            </Col>
+                                                        </Row>
                                                     </Col>
-                                                    <Col sm={10}>
-                                                        {/* {datas.course_name} */}
-                                                        <a href={"/course_map#/course/" + datas["course_id"]}>
-                                                            {/* <Button>more</Button> */}
-                                                            {datas.course_name}
-                                                        </a>
-                                                        {/* <a href={"/course_map/#/course/"}>
-                                                            <Button>more</Button>
-                                                        </a> */}
+                                                    <Col sm={12} md={6} className='map_inside'>
+                                                        <Row className="">
+                                                            <Col sm={3} className='center'>
+                                                                <b className="cognition">{datas.cognition}</b>
+                                                            </Col>
+                                                            <Col sm={3} className='center'>
+                                                                <b className="knowledge">{datas.knowledge}</b>
+                                                            </Col>
+                                                            <Col sm={3} className='center'>
+                                                                <b className="technology">{datas.technology}</b>
+                                                            </Col>
+                                                            <Col sm={3} className='center'>
+                                                                <b className="value">{datas.value}</b>
+                                                            </Col>
+                                                        </Row>
                                                     </Col>
                                                 </Row>
-
-                                            </Col>
-                                            <Col sm={12} md={6} className='color3'>
-                                                <Row>
-                                                    <Col sm={3} className='center'>
-                                                        {datas.cognition}
-                                                    </Col>
-                                                    <Col sm={3} className='center'>
-                                                        {datas.knowledge}
-                                                    </Col>
-                                                    <Col sm={3} className='center'>
-                                                        {datas.technology}
-                                                    </Col>
-                                                    <Col sm={3} className='center'>
-                                                        {datas.value}
-                                                    </Col>
-                                                </Row>
-                                            </Col>
-                                            <Col sm={12}>
-                                                <br />
-                                            </Col>
-                                        </Row>
-                                    );
-                                })}
-                            </Col>
-                        </Row>
-                    );
-                })}
+                                            );
+                                        })}
+                                        <br />
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </div >
+                </div>
             </div >
-        </div >
+        </div>
+
     )
 }
