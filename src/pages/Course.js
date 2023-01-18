e0ea4882e084383c58e8977826884e41af4cf1ab
tyk <110590015@ntut.org.tw>
2023-01-18 15:33:11 +0800

    put data in individual courses

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -9,8 +9,10 @@ import { category } from '../data/map_data'
 
 
 export default function Course() {
-    const id = (window.location.href).slice(-2);
-    // const [data, difficulty] = null;
+    const id = (window.location.href).slice(-2),
+        temp = findData(id),
+        difficulty = temp[0],
+        data = temp[1];
 
     function findData(id) {
         for (var i = 0; i < category.length; i++) {
@@ -22,26 +24,91 @@ export default function Course() {
         }
     }
 
-    const temp = findData(id)
-    const difficulty = temp[0], data = temp[1];
-
-
     return (
         <div className="inner_container">
-            <h1>
-                course name
-            </h1>
-            <p>
-                course id : {id}
-            </p>
-            <p>
-                {difficulty}
-                <br></br>
-                {data.course_name}
+            <Row>
+                <Col sm={3} className='center'>
+                    <h5>
+                        【{difficulty}課程】
+                    </h5>
+                </Col>
+                <Col sm={9}>
+                    <h2>
+                        {data.course_name}
+                    </h2>
+                </Col>
+                <hr></hr>
+                <Col sm={12} className='yellow_word'>
+                    <p><b>{data.course_depiction}</b></p>
+                </Col>
+                <Col sm={4} className='green inside'>
+                    核心能力
+                </Col>
+                <Col sm={8} className='light_green inside'>
+                    <Row>
+                        <Col sm={3} className='center'>
+                            {data.cognition}
+                        </Col>
+                        <Col sm={3} className='center'>
+                            {data.knowledge}
+                        </Col>
+                        <Col sm={3} className='center'>
+                            {data.technology}
+                        </Col>
+                        <Col sm={3} className='center'>
+                            {data.value}
+                        </Col>
+                    </Row>
+                </Col>
 
-            </p>
+                <Col sm={4} className='inside'>
+                    開課時間
+                </Col>
+                <Col sm={8} className='inside'>
+                    {data.course_time.map((time) => {
+                        return (
+                            <Row>
+                                <Col sm={6}>
+                                    課號：{time.id}
+                                </Col>
+                                <Col sm={6}>
+                                    {time.day} &nbsp; {time.time}
+                                </Col>
+                                <Col sm={12}>
+                                    <br />
+                                </Col>
+                            </Row>
+                        );
+                    })}
+                </Col>
 
+                <Col sm={4} className='inside'>
+                    學分數
+                </Col>
+                <Col sm={8} className='inside'>
+                    {data.credit}學分，每週{data.credit}小時
+                </Col>
+                <Col sm={12}>
+                    <br />
+                </Col>
 
-        </div>
+                <Col sm={4} className='inside'>
+                    開課系級
+                </Col>
+                <Col sm={8} className='inside'>
+                    {data.department}
+                </Col>
+                <Col sm={12}>
+                    <br />
+                </Col>
+
+                <Col sm={4} className='inside'>
+                    授課教師
+                </Col>
+                <Col sm={8} className='inside'>
+                    {data.lecturer}
+                </Col>
+            </Row>
+        </div >
     )
 }
