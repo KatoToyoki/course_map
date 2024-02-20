3475886055dce1485e3cc7ae8844ed134b0e2641
tyk <b0984017369@gmail.com>
2024-02-20 23:35:47 +0900

    update courses' information

--- src/data/map_data.js
+++ src/data/map_data.js
@@ -14,14 +14,7 @@ export const category = [
                 與學生專業領域相關應用的專題，學習如何運用程式解決與自身相關領域運用上的問題。",
 
         course_info_en:
-          "In this course, students will learn the basics\
-                 and applications of the Python programming language and establish \
-                 the basic concepts of Python programming design. Through the continuous \
-                 practice, student will construct the basic logic of the program design, \
-                 and the ability of computation think and practice application. \
-                 At the final, a topic related to the application of students' \
-                 professional subjects will be completed in groups, and student will \
-                 learn how to use the program to solve problems in the relevant fields.",
+          "In this course, students will learn the basics and applications of the Python programming language and establish the basic concepts of Python programming design. Through the continuous practice, student will construct the basic logic of the program design, and the ability of computation think and practice application. At the final, a topic related to the application of students' professional subjects will be completed in groups, and student will learn how to use the program to solve problems in the relevant fields.",
         cognition: "1",
         knowledge: "",
         technology: "3",
@@ -32,14 +25,14 @@ export const category = [
         note: "",
         course_time: [
           {
-            semester: "111學年第二學期",
-            id: "317452",
+            semester: "112學年第二學期",
+            id: "331519",
             day: "週一",
             time: "08:00 - 10:00",
           },
           {
-            semester: "",
-            id: "317462",
+            semester: "112學年度第2學期",
+            id: "331633",
             day: "週一",
             time: "10:00 - 12:00",
           },
@@ -60,20 +53,7 @@ export const category = [
                 甚或能開發出更貼合教育情境的模組，還能分享以供大眾使用。",
 
         course_info_en:
-          "In this course we will teach students how to have the \
-                ability for processing big data. It includes how to automatically create \
-                the necessary file environment for storing and referencing the huge amount \
-                of information or big data, which accessed through a programmatic approach \
-                (by Python) to facilitate subsequent search, addition, and deletion to \
-                better fit the application of such data in the educational scenarios and \
-                their own fields. Furthermore, there are many Python data processing \
-                and web scraping modules developed for public use. Thus, we try to \
-                demonstrate how to use these modules to retrieve various types of data \
-                from the Internet and the cloud in a separate way. To help students understand \
-                the usages of the common used modules, this course will provide related \
-                exercises and questions for students to familiarize themselves with these modules. \
-                Hopefully, the students can learn, and then apply these contents to their \
-                own fields and future educational big data projects.",
+          "In this course we will teach students how to have the ability for processing big data. It includes how to automatically create the necessary file environment for storing and referencing the huge amount of information or big data, which accessed through a programmatic approach (by Python) to facilitate subsequent search, addition, and deletion to better fit the application of such data in the educational scenarios and their own fields. Furthermore, there are many Python data processing and web scraping modules developed for public use. Thus, we try to demonstrate how to use these modules to retrieve various types of data from the Internet and the cloud in a separate way. To help students understand the usages of the common used modules, this course will provide related exercises and questions for students to familiarize themselves with these modules. Hopefully, the students can learn, and then apply these contents to their own fields and future educational big data projects.",
         cognition: "1",
         knowledge: "2",
         technology: "3",
@@ -85,8 +65,8 @@ export const category = [
         note: "",
         course_time: [
           {
-            semester: "111學年第二學期",
-            id: "316921",
+            semester: "112學年第二學期",
+            id: "327426",
             day: "週四",
             time: "16:00 - 19:00",
           },
@@ -136,15 +116,7 @@ export const category = [
                     完成本門課程的學生將能熟悉教育領域中主要與最新進的統計分析技術，並同時掌握透過編寫程式的方式執行數據分析的能力。",
 
         course_info_en:
-          "This course aims to give students the opportunity to study statistics and its application \
-                on the field.The first section would introduce basic statistical concepts including sampling, \
-                distribution, measurement, among others.The second section would introduce regression methods.\
-                Students would also learn how to conduct data analysis using Stata, the most powerful software \
-                in econometrics.The third section would introduce multivariate statistics, including factor analysis \
-                and cluster analysis.In addition to handling administrative data, students would also learn the concept \
-                of latent variable and its application.After finishing this course, students would not only know \
-                the most advanced data analytics techniques but also have the ability to do programing for \
-                data analysis tasks.",
+          "This course aims to give students the opportunity to study statistics and its application on the field. The first section would introduce basic statistical concepts including sampling, distribution, measurement, among others. The second section would introduce regression methods. Students would also learn how to conduct data analysis using Statistics software, the most powerful software in econometrics. The third section would introduce multivariate statistics, including factor analysis and cluster analysis. In addition to handling administrative data, students would also learn the concept of latent variable and its application. After finishing this course, students would not only know the most advanced data analytics techniques but also have the ability to do programing for data analysis tasks.",
 
         cognition: "",
         knowledge: "2",
@@ -155,9 +127,9 @@ export const category = [
         course_time: [
           {
             semester: "112學年第一學期",
-            id: "",
-            day: "",
-            time: "開課時間：待公告",
+            id: "324649",
+            day: "週三",
+            time: "10:00 - 12:00",
           },
         ],
         department: "通識中心",
@@ -183,19 +155,7 @@ export const category = [
                 在實作專題中課程亦著重學生報告能力的培養，希望培育出全方位的資料科學人才。",
 
         course_info_en:
-          "Data science is an emerging interdisciplinary field \
-                that explores how to produce or create the valuable information and knowledge \
-                from large, multi-typed, or unstructured data and provides effective feedbacks \
-                to many applications, including education. The related topics include data \
-                processing and computing, data storage, statistics and probability, \
-                linear algebra, and data analysis. This course introduces the rapidly growing field, \
-                discusses the state-of-art techniques, and provides students with the fundamental \
-                related knowledge as well as the ability to use software tools. The major \
-                phases on data manipulation- collection, preparation, analytics, and presentation, \
-                in data science will be discussed. The training of this course is toward equipping \
-                students with the general mindset and the ability to solve problems practically \
-                with the learned techniques on educational data with project groups. In addition, \
-                the students also need to practice the presentation for the findings and result",
+          "Data science is an emerging interdisciplinary field that explores how to produce the valuable knowledge from large, multi-typed, or unstructured data and provides effective feedbacks to many applications, including education. The topics include data processing, data storage, statistics and probability, linear algebra, and data analysis. The course introduces the rapidly growing field, discusses the state-of-art techniques, and provides students with fundamental related knowledge and the ability to use software tools. The major phases on data manipulation-collection, preparation, analytics, and presentation, in data science will be discussed. The course training is toward equipping students with the mindset and the ability to solve problems with the learned techniques on educational data. In addition, students need to practice the presentation for the findings of the studied problem in order to equip with the ability to convey the thoughts well and demonstrate things visually.",
 
         cognition: "1",
         knowledge: "2",
@@ -207,10 +167,10 @@ export const category = [
         note: "備註：資工系同學不可認列跨領域學習畢業之條件。",
         course_time: [
           {
-            semester: "111學年第二學期",
-            id: "316310",
+            semester: "112學年第二學期",
+            id: "328978",
             day: "週一",
-            time: "13:00 - 16:00",
+            time: "09:00 - 12:00",
           },
         ],
         department: "資訊工程系",
@@ -229,7 +189,8 @@ export const category = [
                 以獨立生成有意義的數據結構；最後則聚焦在教育方案的效果分析，以及將數據分析資料視覺化，\
                 輔以淺顯易懂的數據解讀報告，增加學員對於數據分析結果的瞭解。",
 
-        course_info_en: "",
+        course_info_en:
+          "Institutional research is the field where the data science can apply in the education. In the educational field, institutional researchers must use their knowledge and techniques in the educational data analytics, providing the scientific report in a friendly manner to their affiliation. Those scientific report is aimed to improve the institutional operation and the teaching activities. This course would first introduce the function of each office at the university and the institution research topic those offices have interest. Then, the data structure which is commonly seen in the educational field would be introduced followed by a series of training on the data manipulation. Last, this course will teach the students how to conduct the educational program evaluation, data visualization, data analysis and the result interpretation.",
         cognition: "1",
         knowledge: "2",
         technology: "3",
@@ -238,10 +199,10 @@ export const category = [
         note: "",
         course_time: [
           {
-            semester: "112學年第一學期",
-            id: "",
-            day: "",
-            time: "開課時間：待公告",
+            semester: "112學年第二學期",
+            id: "332507, 332526",
+            day: "週一",
+            time: "14:00 - 17:00",
           },
         ],
         department: "教務處 (校院級課程)",
@@ -256,7 +217,8 @@ export const category = [
                 並運用實際工具如: Weka, Python等，針對教育大數據進行分析。透過程式練習以及期末專題，\
                 能讓學生深入理解教育資料分析的基本原理以及實際工具的應用。",
 
-        course_info_en: "",
+        course_info_en:
+          "This course is one of the advanced courses in the Micro Courses on Educational Big Data. It introduces the basic concepts and principles of data mining, including: data preprocessing, frequent pattern mining, classification, and clustering. Also, students need to use tools such as Weka and Python for educational big data analytics. With the programming exercises and term project, students will be able to learn the fundamental ideas of educational data analysis and real applications with practical tools.",
         cognition: "1",
         knowledge: "",
         technology: "3",
@@ -266,9 +228,9 @@ export const category = [
         course_time: [
           {
             semester: "112學年第一學期",
-            id: "",
-            day: "",
-            time: "開課時間：待公告",
+            id: "323351",
+            day: "週二, 週三",
+            time: "13:00 - 15:00, 09:00 - 10:00",
           },
         ],
         department: "資訊工程系",
@@ -286,18 +248,7 @@ export const category = [
                 以及資訊安全相關法規以及個人資料保護法，並說明個資的去識別化做法，以落實個人資料保護。",
 
         course_info_en:
-          "English Description With the emergence of a large number\
-                 of information security incidents, the importance of information security\
-                  is highlighted. The application of information security in education is\
-                   the basic course of information security. The concepts of information \
-                   security and personal data protection are applied in practical courses, \
-                   so that students who are new to the field of information security can \
-                   understand what information security is and have a more correct \
-                   understanding of information security. In addition, this course introduces \
-                   the direction of practical environment in information security education. \
-                   As well as information security-related regulations and personal \
-                   data protection regulations, and explain the de-identification of \
-                   personal data to perform personal data protection.",
+          "With the emergence of a large number of information security incidents, the importance of information security is highlighted. The application of information security in education is the basic course of information security. The concepts of information security and personal data protection are applied in practical courses, so that students who are new to the field of information security can understand what information security is and have a more correct understanding of information security. In addition, this course introduces the direction of practical environment in information security education. As well as information security-related regulations and personal data protection regulations, and explain the de-identification of personal data to perform personal data protection. orm personal data protection.",
         cognition: "1",
         knowledge: "2",
         technology: "3",
@@ -306,8 +257,8 @@ export const category = [
         note: "備註：資工系與資財系同學不可認列跨領域學習畢業之條件。",
         course_time: [
           {
-            semester: "111學年第二學期",
-            id: "317313",
+            semester: "112學年第二學期",
+            id: "327524",
             day: "週五",
             time: "09:00 - 12:00",
           },
@@ -334,7 +285,8 @@ export const category = [
                 模擬實驗及最後的研究結論與模型驗證。最後，透過特定主題教案的設計，讓學習者進一步的去思考數據\
                 分析的邏輯，從教學者的角度更深入理解教育大數據分析。",
 
-        course_info_en: "",
+        course_info_en:
+          "This course focuses on the integration of information analysis theory and practice. As such, students will be gradually introduced to relevant analysis theories and algorithms. By using Rapidminer, students can easily complete the proper analysis process by dragging and dropping operating components. This software allows students to generate a diverse range of visual presentations, strengthening their ability to operate related software through practical application. Additionally, this course will work in conjunction with educational institutions and industries to address real-world problems and integrate school research data in special courses. Students will participate in school affairs analysis and research, considering issues related to personal data and information security. The course will also involve planning to de-identify data in the school, which will be used in the classroom after being altered via code. This approach will allow students to conduct problem analysis, model",
         cognition: "1",
         knowledge: "",
         technology: "3",
@@ -344,9 +296,9 @@ export const category = [
         course_time: [
           {
             semester: "112學年第一學期",
-            id: "",
-            day: "",
-            time: "開課時間：待公告",
+            id: "325050",
+            day: "週二",
+            time: "13:00 - 16:00",
           },
         ],
         department: "教務處 (校院級課程)",
@@ -362,7 +314,8 @@ export const category = [
                 資料於專題課程，參與校務分析研究：考量資安的個資議題，擬將校內部份去識別化後之資料，\
                 經過編碼變造處理後於課堂使用，以教育相關大數據進行專題實作與分析。透過分組討論以及專題實作競賽，\
                 讓學生實際應用人工智慧工具與方法於教育資料分析。",
-        course_info_en: "",
+        course_info_en:
+          "The goal of this course is to introduce the technical topics and share industrial examples related to artificial intelligence, including: knowledge representation, machine learning, and deep learning. The students are required to utilize machine learning tools such as: TensorFlow, Keras, and PyTorch, to participate in the institutional repository research and analysis of related data in term projects. In consideration of privacy issues of personal information, the data will go through de-identification process for the usage purpose of educational big data analytics in term projects. With group discussions and project competition, students will be able to apply artificial intelligence tools and methods in educational data anlaysis.",
         cognition: "1",
         knowledge: "",
         technology: "3",
@@ -372,13 +325,13 @@ export const category = [
         course_time: [
           {
             semester: "112學年第二學期",
-            id: "",
-            day: "",
-            time: "開課時間：待公告",
+            id: "332504, 332527",
+            day: "週二",
+            time: "13:00 - 16:00",
           },
         ],
         department: "教務處 (校院級課程)",
-        lecturer: "王正豪教授、劉傳銘教授、傅遠志教授",
+        lecturer: "王正豪教授、劉傳銘教授",
       },
     ],
   },
